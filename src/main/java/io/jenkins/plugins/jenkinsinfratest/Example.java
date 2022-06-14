72757272516cd120527f2969dab28728001b1311
Tim Jacomb <timjacomb1+github@gmail.com>
2022-06-14 17:51:36 +0100

    Add dummy class

---
+++ src/main/java/io/jenkins/plugins/jenkinsinfratest/Example.java
@@ -0,0 +1,15 @@
+package io.jenkins.plugins.jenkinsinfratest;
+
+/**
+ * An example class with some javadoc
+ */
+public class Example {
+
+    /**
+     * I do not do anything
+     * @param param ignored
+     */
+    public void doesNothing(String param) {
+        // does nothing
+    }
+}
