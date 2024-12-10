1d12ef7358e3393836122b876cd100f9f2ac03da
Daniel Beck <daniel-beck@users.noreply.github.com>
2024-12-10 17:40:58 +0100

    More tests

---
+++ src/test/java/io/jenkins/plugins/jenkinsinfratest/UpdateCenter3Test.java
@@ -0,0 +1,102 @@
+package io.jenkins.plugins.jenkinsinfratest;
+
+import io.jenkins.plugins.casc.misc.ConfiguredWithCode;
+import io.jenkins.plugins.casc.misc.JenkinsConfiguredWithCodeRule;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class UpdateCenter3Test {
+    @Rule
+    public JenkinsConfiguredWithCodeRule j = new JenkinsConfiguredWithCodeRule();
+
+    @Before
+    public void prepare() throws Exception {
+        j.jenkins.getUpdateCenter().updateAllSites();
+
+        assertThat(j.jenkins.getPlugin("opentelemetry"), notNullValue());
+    }
+
+    @ConfiguredWithCode("configuration-as-code.yml")
+    @Test
+    public void test() throws Exception {
+    }
+
+    @ConfiguredWithCode("configuration-as-code.yml")
+    @Test
+    public void test2() throws Exception {
+    }
+
+    @ConfiguredWithCode("configuration-as-code.yml")
+    @Test
+    public void test3() throws Exception {
+    }
+
+    @ConfiguredWithCode("configuration-as-code.yml")
+    @Test
+    public void test4() throws Exception {
+    }
+
+    @ConfiguredWithCode("configuration-as-code.yml")
+    @Test
+    public void test5() throws Exception {
+    }
+
+    @ConfiguredWithCode("configuration-as-code.yml")
+    @Test
+    public void test6() throws Exception {
+    }
+
+    @ConfiguredWithCode("configuration-as-code.yml")
+    @Test
+    public void test7() throws Exception {
+    }
+
+    @ConfiguredWithCode("configuration-as-code.yml")
+    @Test
+    public void test8() throws Exception {
+    }
+
+    @ConfiguredWithCode("configuration-as-code.yml")
+    @Test
+    public void test9() throws Exception {
+    }
+
+    @ConfiguredWithCode("configuration-as-code.yml")
+    @Test
+    public void test10() throws Exception {
+    }
+
+    @ConfiguredWithCode("configuration-as-code.yml")
+    @Test
+    public void test11() throws Exception {
+    }
+
+    @ConfiguredWithCode("configuration-as-code.yml")
+    @Test
+    public void test12() throws Exception {
+    }
+
+    @ConfiguredWithCode("configuration-as-code.yml")
+    @Test
+    public void test13() throws Exception {
+    }
+
+    @ConfiguredWithCode("configuration-as-code.yml")
+    @Test
+    public void test14() throws Exception {
+    }
+
+    @ConfiguredWithCode("configuration-as-code.yml")
+    @Test
+    public void test15() throws Exception {
+    }
+
+    @ConfiguredWithCode("configuration-as-code.yml")
+    @Test
+    public void test16() throws Exception {
+    }
+}
