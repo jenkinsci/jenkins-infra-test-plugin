168a8cdbd13a91d372033e443a82658b711d18c2
Daniel Beck <daniel-beck@users.noreply.github.com>
2024-12-10 13:37:33 +0100

    Add configuration

--- src/test/java/io/jenkins/plugins/jenkinsinfratest/UpdateCenterTest.java
+++ src/test/java/io/jenkins/plugins/jenkinsinfratest/UpdateCenterTest.java
@@ -1,5 +1,6 @@
 package io.jenkins.plugins.jenkinsinfratest;
 
+import io.jenkins.plugins.casc.misc.ConfiguredWithCode;
 import io.jenkins.plugins.casc.misc.JenkinsConfiguredWithCodeRule;
 import org.junit.Rule;
 import org.junit.Test;
@@ -8,6 +9,7 @@ public class UpdateCenterTest {
     @Rule
     public JenkinsConfiguredWithCodeRule j = new JenkinsConfiguredWithCodeRule();
 
+    @ConfiguredWithCode("configuration-as-code.yml")
     @Test
     public void test() throws Exception {
         j.jenkins.getUpdateCenter().updateAllSites();
